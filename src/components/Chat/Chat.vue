bd26a9da6484fce077394294988390dc528b8e92
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-11-18 21:50:07 +0100

    feat: show new comment on top

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -45,7 +45,7 @@
   <!--Chat.vue-->
   <div>
     <ul>
-        <li v-for="m in allMessages.data.slice(-20)" :key="m.id">
+        <li v-for="m in allMessages.data.slice(-20).reverse()" :key="m.id">
         {{ m.text }}
       </li>
     </ul>
